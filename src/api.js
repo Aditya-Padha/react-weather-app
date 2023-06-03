3c6018d4d37ae0e69fd2dc599574ae1843a31f98
Aditya Padha <81345889+Aditya-Padha@users.noreply.github.com>
2023-06-04 03:08:42 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,11 +3,11 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "ececcb3e98msh273220179331c2bp1a1fe9jsn245628ef5413",
+    "X-RapidAPI-Key": "",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
 
-export const WEATHER_API_KEY = "b72e9f6cfa971b3c4c5d36d376ca6f7d";
+export const WEATHER_API_KEY = "";
